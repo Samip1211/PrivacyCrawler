b70d6fe8c481186e653902d7dfce6d49ced1553c
giok1992 <giok13@me.com>
2017-11-13 20:28:09 -0600

    added db insertion/terms

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import sqlite3
+import sys
+
 
 def parse(page):
      soup = BeautifulSoup(page, 'html.parser')
@@ -9,19 +12,75 @@ def parse(page):
 
 
 
-req = requests.get("https://www.stackoverflow.com/") # amazon,apple,facebook, youtube, 
+def findPrivacy(domain):
+	
 
-page = req.text
+	req = requests.get(domain) # amazon,apple,facebook, youtube, 
 
-soup = parse(page)
+	page = req.text
 
-Bodycontent = soup.find("body")
+	soup = parse(page)
 
-#print(content)
+	Bodycontent = soup.find("body")
 
-for div in Bodycontent.find_all("div"):
+	#print(Bodycontent)
 
-    #print(div) 
 
-    for privacy in div.find_all("a",text="Privacy"):
-        print(privacy.get('href'))    
+	for div in Bodycontent.find_all("div"):
+
+	    #print(div) 
+	        for privacy in div.find_all("a",text="Privacy Policy"):
+	                privacyURL = privacy.get('href').encode('ascii','ignore')
+	                if(privacyURL.find(domain)):
+	                        return privacyURL
+def findToS(domain):
+
+	req = requests.get(domain) # amazon,apple,facebook, youtube, 
+
+	page = req.text
+
+	soup = parse(page)
+
+	Bodycontent = soup.find("body")
+
+	#print(Bodycontent)
+
+
+	for div in Bodycontent.find_all("div"):
+
+	    #print(div) 
+	        for terms in div.find_all("a",text="Legal"):
+	                termsURL = terms.get('href').encode('ascii','ignore')
+	                if(termsURL.find(domain)):
+	                        return termsURL
+
+def insertDB(domain, privacy, terms):
+
+	conn = sqlite3.connect('/var/www/html/tos_scraper/tos_scraper.db')
+
+	try:
+		conn.execute("INSERT INTO website(tos, privacy, url) VALUES (?, ?, ?)", [terms, privacy, domain])
+
+		conn.commit()
+	except sqlite3.Error as er:
+		print 'er:', er.message
+	conn.close()
+
+
+def main():
+
+	domain = sys.argv[1]
+	print(domain)
+	privacy = findPrivacy(domain)
+
+	terms = findToS(domain)
+
+	print(terms)
+	print(privacy)
+
+	insertDB(domain, privacy, terms)
+
+
+main()
+
+
