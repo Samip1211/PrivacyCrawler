a81cdb271496b022706ba5f4f1d724567613ab82
giok1992 <giok13@me.com>
2017-11-15 19:58:14 -0600

    Version 0.1

--- index.php
+++ index.php
@@ -1,43 +1,12 @@
-html>
-<head>
-<style>
-table, th, td {
-    border: 1px solid black;
-}</style>
-</head>
-<body>
-
-<h1> Please enter the website you would like to scrape below</h1>
-
-<form action="scrapper.php" method="post">
-        <input type="text" value="domain" name="domain" />
-        <input type="submit" value="Send" />
-</form> 
-
-<table style="width=100%">
-<tr>
-<th>id</th>
-<th>Website URL</th>
-<th>Website Privacy Link</th>
-<th>Website Terms Of Service</th>
-
 <?php
 
 $db = new SQLite3('tos_scraper.db');
 
-$results = $db->query("SELECT id, url, privacy, tos FROM website;");
+$query = $db->query("SELECT id, url, privacy, tos FROM website;");
 
- 
-while ($row = $results->fetchArray()) {
-echo '<tr>';
-echo '<td>' . $row['id'] . '</td><td>' . $row['url']  . '</td><td>' . $row['privacy'] . '</td><td>' . $row['tos'] . '</td>';
-echo '</tr>';
+
+foreach ($query as $entry) {
+    echo 'ID: ' . $entry['id'] . '  url: ' . $entry['url']  . '  privacy: ' . $entry['privacy'] . '  tos: ' . $entry['tos'];
 }
 
-
-
 ?>
-</tr>
-</table>
-</body>
-</html>
--- scrapper.py
+++ scrapper.py
@@ -3,84 +3,176 @@ import re
 from bs4 import BeautifulSoup
 import sqlite3
 import sys
+import fnmatch
+import urllib.request
+import reader
+import json
+
+# UserAgent,
 
 
+# sets up parser using beautifulsoup
 def parse(page):
-     soup = BeautifulSoup(page, 'html.parser')
-     
-     return soup;
+    soup = BeautifulSoup(page, 'html.parser')
+
+    return soup;
+
+
+# searches for terms and condition
+"""
+def search_condition(Bodycontent):
+
+    for div in Bodycontent.find_all("div"):
+
+        for terms in div.find_all("a",text=re.compile("Condition")):
+            termsURL = terms.get('href').encode('ascii','ignore')
+
+            print(termsURL)
+            return termsURL
+
+"""
+
+##This function will take in the domain and a term, it will then try to find that term anywhere on the page using Mercury API wrapper
+
+def confirm_Page(domain, term):
+
+
+    body = reader.reader(domain, "OJ6jo9V0rAu5OITm4SbhzWHrJcKyzDtaB8s4INic", 720)
+
+    d = json.loads(str(body))
+    if(term in str((d['content']))):
+        return True
+
+    else:
+        return False
 
 
 
-def findPrivacy(domain):
-	
 
-	req = requests.get(domain) # amazon,apple,facebook, youtube, 
+## Finds the <body> of a html file
+def get_Body_Content(domain):
+    req = requests.get(domain)  # amazon,apple,facebook, youtube,
 
-	page = req.text
+    page = req.text
 
-	soup = parse(page)
+    soup = parse(page)
 
-	Bodycontent = soup.find("body")
+    Bodycontent = soup.find("body")
 
-	#print(Bodycontent)
-
-
-	for div in Bodycontent.find_all("div"):
-
-	    #print(div) 
-	        for privacy in div.find_all("a",text="Privacy Policy"):
-	                privacyURL = privacy.get('href').encode('ascii','ignore')
-	                if(privacyURL.find(domain)):
-	                        return privacyURL
-def findToS(domain):
-
-	req = requests.get(domain) # amazon,apple,facebook, youtube, 
-
-	page = req.text
-
-	soup = parse(page)
-
-	Bodycontent = soup.find("body")
-
-	#print(Bodycontent)
+    return Bodycontent
 
 
-	for div in Bodycontent.find_all("div"):
+## Grabs the robots text file from the webserver. We will use this to detect whether or
+## not website administrator will allow our bot to crawl their page by checking if / is disallowed
 
-	    #print(div) 
-	        for terms in div.find_all("a",text="Legal"):
-	                termsURL = terms.get('href').encode('ascii','ignore')
-	                if(termsURL.find(domain)):
-	                        return termsURL
 
+def read_robot_file(domain):
+    req = urllib.request.urlopen(domain + "/robots.txt")  #requests the robots file
+
+    code = req.code                                       #checks to see if the file exists. If it doesn't exist, return true
+    if (code / 100 >= 4):
+        return True
+
+    fileContent = req.read().splitlines()
+    x = 0
+    for line in fileContent:                            #goes through the robots file on by one looking for our useragent
+        x += 1
+        l = line.find(b'User-agent: tos-scrapper')
+        if (l >= 0):
+            print(fileContent[x])
+            if (fileContent[x] in 'Disallowed: /'):     #if it shows we are disallowed, return False
+                return False
+    return True
+
+
+## This function takes in the html of the body, and keyword we are searching for that is contained in a hyperlink. It returns the
+## url that is attached to the hyperlink text
+
+def findToS(Bodycontent, keyword):
+    termsURL = ""
+    for div in Bodycontent.find_all("div"):
+        for terms in div.find_all("a", text=re.compile(keyword)):
+            termsURL = terms.get('href').encode('ascii', 'ignore')
+            return termsURL
+
+    if not termsURL:
+        return False
+
+
+##inserts the website tos/privacy and domain if the information is found.
 def insertDB(domain, privacy, terms):
+    conn = sqlite3.connect('tos_scraper.db')
 
-	conn = sqlite3.connect('/var/www/html/tos_scraper/tos_scraper.db')
+    try:
+        conn.execute("INSERT INTO website(tos, privacy, url) VALUES (?, ?, ?)", [terms, privacy, domain])
 
-	try:
-		conn.execute("INSERT INTO website(tos, privacy, url) VALUES (?, ?, ?)", [terms, privacy, domain])
+        conn.commit()
+    except sqlite3.Error as er:
+        print('er:' + er.message)
+    conn.close()
 
-		conn.commit()
-	except sqlite3.Error as er:
-		print 'er:', er.message
-	conn.close()
+
+## goes through a keyword list stored in file variable and then calls the findToS function with each keyword. Trying to find
+## the keyword on the page inside a hyperlink text.
+def checkKeyWords(url, file):
+    myfile = open(file, "r").read().split('\n')
+
+    body = get_Body_Content(url)
+
+    if len(myfile) != 0:
+        for line in myfile:
+            # print(line)
+            policy_url = findToS(body, line)
+            if (policy_url != False):
+                return policy_url
+
+    return False
+
 
 
 def main():
+    domain = "https://stackoverflow.com"
 
-	domain = sys.argv[1]
-	print(domain)
-	privacy = findPrivacy(domain)
+    if(read_robot_file("https://stackoverflow.com")):
 
-	terms = findToS(domain)
 
-	print(terms)
-	print(privacy)
 
-	insertDB(domain, privacy, terms)
+        content = get_Body_Content(domain)
+
+        # print(content)
+
+
+        terms = checkKeyWords(domain, "terms.txt")          #look for terms and conditions
+        privacy = checkKeyWords(domain, "privacy.txt")      #look for privacy policy
+
+
+        if terms is None:
+            print("Could not find the terms of service")
+
+        if privacy is None:
+            print("Could not find Privacy Policy")
+
+        print(terms)
+        print(privacy)
+
+
+        if(confirm_Page(terms, "Terms of Service")):
+            print("Terms of Service confirmed")
+        else:
+            print("Terms of service url might be a false positive")
+
+        if(confirm_Page(privacy, "Privacy Policy")):
+            print("Privacy Policy Confirmed")
+        else:
+            print("Privac Policy url might be a false positive")
+
+
+    else:
+
+        print("The website is not allowing us to scrape their main page")
+
+
+    insertDB(domain, privacy, terms)              #after we find the privacy and terms, we can insert it into database
 
 
 main()
-
-
